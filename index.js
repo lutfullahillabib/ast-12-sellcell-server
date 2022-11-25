0b3a2a22ad5ba0965194f96b6829ca97dda880e1
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-25 22:40:37 +0600

    strip req

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const jwt = require('jsonwebtoken');
 
 require('dotenv').config();
 
-
-// 
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
 const port = process.env.PORT || 5000;
