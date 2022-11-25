7e59cd29bf7775b4e665e214eadf35dc5ae9ff27
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-25 22:37:43 +0600

    start

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const jwt = require('jsonwebtoken');
-
-require('dotenv').config();
-
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// 
 
 const port = process.env.PORT || 5000;
 
@@ -17,53 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASS);
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jjkmnej.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri);
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-// function verifyJWT(req, res, next) {
-
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         return res.status(401).send('unauthorized access');
-//     }
-
-//     const token = authHeader.split(' ')[1];
-
-//     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-//         if (err) {
-//             return res.status(403).send({ message: 'forbidden access' })
-//         }
-//         req.decoded = decoded;
-//         next();
-//     })
-
-// }
-
-
-
-// async function run() {
-//     try {
-
-
-//     }
-
-
-//     finally {
-
-//     }
-// }
-
-// run().catch(console.log);
-
-
+// 
 
 app.get('/', async (req, res) => {
     res.send('ast-12-sellcell server is running');
