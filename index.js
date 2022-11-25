c518e7c5602e59c90170f2cf6f4f8a258f7c681c
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-25 23:04:18 +0600

    jjwwtt

--- index.js
+++ index.js
@@ -29,6 +29,26 @@ console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// function verifyJWT(req, res, next) {
+
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) {
+//         return res.status(401).send('unauthorized access');
+//     }
+
+//     const token = authHeader.split(' ')[1];
+
+//     jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+//         if (err) {
+//             return res.status(403).send({ message: 'forbidden access' })
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+
+// }
+
+
 
 // 
 
