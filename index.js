42d16623656440f470887127fc772531a2a6114a
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-25 22:39:10 +0600

    mongo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 // 
+
 
 const port = process.env.PORT || 5000;
 
