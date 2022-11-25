bb0974f0bb9fd01a4b9d99c5f1d4beb93e556060
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-25 23:03:39 +0600

    client connect

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 console.log(uri);
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 // 
 
