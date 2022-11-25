3ca2b9ca36878bb1e32b3bfd9baa1f8f85b7971d
lutfullahillabib <lutfullahillabib@gmail.com>
2022-11-25 23:08:19 +0600

    user put

--- index.js
+++ index.js
@@ -65,6 +65,25 @@ async function run() {
         });
 
 
+        app.put("/user", async (req, res) => {
+            const email = req.body.email;
+            const data = req.body;
+            const query = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: data.name,
+                    email: data.email,
+                    photoURL: data.photoURL,
+                    role: data.role,
+                    verify: data.verify,
+                },
+            };
+            const result = await userCollection.updateOne(query, updateDoc, options);
+            res.send(result);
+        });
+
+
 
     }
 
